d0c009ffdc9e4dad330178dd66025ddafa570302
oleksandr kazimirsky <oleksandr_kazimirsky@epam.com>
2017-03-19 15:52:11 +0200

    HelperClass added

---
+++ Collection_prelim_ver/HelperClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_prelim_ver
+{
+    class HelperClass
+    {
+        public void GetHelp()
+        { }
+    }
+}
